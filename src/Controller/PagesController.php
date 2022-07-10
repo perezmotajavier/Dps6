c3f3d6ab122b0a04f4ed2b1db3e70b21244a7baa
perezmotajavier <gabyperezmota@icloud.com>
2022-07-10 22:59:34 +0200

    [WIP] Add logic to generate password

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -13,18 +13,42 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-
         return $this->render('pages/home.html.twig');
     }
 
-    /**
-     *
-     */
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request)
     {
-        dump($request->query->all());
-        return $this->render('pages/password.html.twig');
+        $lenght = $request->query->getInt('lenght');
+        $uppercaseLetters = $request->query->getBoolean('uppercase_letters');
+        $digits = $request->query->getBoolean('digits');
+        $specialCharacters = $request->query->getBoolean('special_characters');
+
+        // Default character
+        $characters = range('a', 'z');
+
+        if ($uppercaseLetters){
+            $characters = array_merge($characters, range('A', 'Z'));
+        }
+        if ($digits) {
+            $characters = array_merge($characters, range(0, 9));
+        }
+        if ($specialCharacters) {
+            $characters = array_merge($characters, ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']);
+
+        }
+
+        $password = '';
+
+        for ($i = 0; $i < $lenght; $i++) {
+            $password = $password . $characters[mt_rand(0, count($characters) - 1)];
+        }
+
+
+        return $this->render('pages/password.html.twig', [
+            'password' => $password
+        ]);
+
 
     }
 }
