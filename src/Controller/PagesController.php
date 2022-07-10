1f02cd003cb6bfd665f54b1750bfd150a28e3d5a
perezmotajavier <gabyperezmota@icloud.com>
2022-07-10 17:29:16 +0200

    [WIP] Add generatePassword and basic password template

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,18 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
+
         return $this->render('pages/home.html.twig');
+    }
+
+    /**
+     *
+     */
+    #[Route('/generate-password', name: 'app_generate_password')]
+    public function generatePassword(Request $request)
+    {
+        dump($request->query->all());
+        return $this->render('pages/password.html.twig');
+
     }
 }
