344a55e373311c3dcd56d718a3524c0f8af6a6ef
perezmotajavier <gabyperezmota@icloud.com>
2022-07-19 14:40:41 +0200

    [WIP] Comment code because we are good developers

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -23,6 +23,9 @@ class PagesController extends AbstractController
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request, PasswordGenerator $passwordGenerator): Response
     {
+        // We make sure that the password length is always
+        // at minimum {app.password_min_length}
+        // and at maximum {app.password_max_length}.
         $length = max(
             min($request->query->getInt('length'), $this->getParameter('app.password_max_length')),
             $this->getParameter('app.password_min_length')
