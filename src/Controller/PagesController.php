3803b89a9c577fdab4e73307ebdcdc1e954efdfe
perezmotajavier <gabyperezmota@icloud.com>
2022-07-25 00:40:35 +0200

    [WIP] Add Cookie

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Service\PasswordGenerator;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class PagesController extends AbstractController
 {
@@ -14,7 +16,7 @@ class PagesController extends AbstractController
     public function home(): Response
     {
         return $this->render('pages/home.html.twig', [
-            'password_default_length' =>  $this->getParameter('app.password_default_length'),
+            'password_default_length' => $this->getParameter('app.password_default_length'),
             'password_min_length' => $this->getParameter('app.password_min_length'),
             'password_max_length' => $this->getParameter('app.password_max_length')
         ]);
@@ -30,16 +32,29 @@ class PagesController extends AbstractController
             min($request->query->getInt('length'), $this->getParameter('app.password_max_length')),
             $this->getParameter('app.password_min_length')
         );
+        $UppercaseLetters = $request->query->getBoolean('uppercase_letters');
+        $digits = $request->query->getBoolean('digits');
+        $SpecialCharacters = $request->query->getBoolean('special_characters');
 
 
         $password = $passwordGenerator->generate(
             $length,
-            $request->query->getBoolean('uppercase_letters'),
-            $request->query->getBoolean('digits'),
-            $request->query->getBoolean('special_characters')
+            $UppercaseLetters,
+            $digits,
+            $SpecialCharacters
         );
 
 
-        return $this->render('pages/password.html.twig', compact('password'));
+        $response = $this->render('pages/password.html.twig', compact('password'));
+
+        $response->headers->setCookie(cookie: Cookie::create('app_length', $length, new DateTimeImmutable('+5 years')));
+
+        $response->headers->setCookie(Cookie::create('app_uppercase_letters', $UppercaseLetters ? '1' : '0', new DateTimeImmutable('+5 years')));
+
+        $response->headers->setCookie(Cookie::create('app_digits', $digits ? '1' : '0', new DateTimeImmutable('+5 years')));
+
+        $response->headers->setCookie(Cookie::create('app_special_characters', $SpecialCharacters ? '1' : '0', new DateTimeImmutable('+5 years')));
+
+        return $response;
     }
 }
