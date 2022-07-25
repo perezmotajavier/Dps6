7cf7fa10b434b243f58f76d39bc45ebdc44749c7
perezmotajavier <gabyperezmota@icloud.com>
2022-07-25 22:46:35 +0200

    [WIP] Prepopulate form cookies values

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -13,12 +13,22 @@ use Symfony\Component\HttpFoundation\Cookie;
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(Request $request): Response
+
     {
-        return $this->render('pages/home.html.twig', [
-            'password_default_length' => $this->getParameter('app.password_default_length'),
-            'password_min_length' => $this->getParameter('app.password_min_length'),
-            'password_max_length' => $this->getParameter('app.password_max_length')
+        return $this->render(view: 'pages/home.html.twig', parameters: [
+
+            'password_default_length' => $request->cookies->getInt(
+                key: 'app_length', default: $this->getParameter(name: 'app.password_default_length')),
+
+            'uppercase_letters' => $request->cookies->getBoolean(key: 'app_uppercase_letters'),
+
+            'digits' => $request->cookies->getBoolean(key: 'app_digits'),
+
+            'special_characters' => $request->cookies->getBoolean(key: 'app_special_characters'),
+
+            'password_min_length' => $this->getParameter(name: 'app.password_min_length'),
+            'password_max_length' => $this->getParameter(name: 'app.password_max_length')
         ]);
     }
 
@@ -29,12 +39,12 @@ class PagesController extends AbstractController
         // at minimum {app.password_min_length}
         // and at maximum {app.password_max_length}.
         $length = max(
-            min($request->query->getInt('length'), $this->getParameter('app.password_max_length')),
-            $this->getParameter('app.password_min_length')
+            min($request->query->getInt(key: 'length'), $this->getParameter(name: 'app.password_max_length')),
+            $this->getParameter(name: 'app.password_min_length')
         );
-        $UppercaseLetters = $request->query->getBoolean('uppercase_letters');
-        $digits = $request->query->getBoolean('digits');
-        $SpecialCharacters = $request->query->getBoolean('special_characters');
+        $UppercaseLetters = $request->query->getBoolean(key: 'uppercase_letters');
+        $digits = $request->query->getBoolean(key: 'digits');
+        $SpecialCharacters = $request->query->getBoolean(key: 'special_characters');
 
 
         $password = $passwordGenerator->generate(
@@ -45,15 +55,15 @@ class PagesController extends AbstractController
         );
 
 
-        $response = $this->render('pages/password.html.twig', compact('password'));
+        $response = $this->render(view: 'pages/password.html.twig', parameters: compact(var_name: 'password'));
 
-        $response->headers->setCookie(cookie: Cookie::create('app_length', $length, new DateTimeImmutable('+5 years')));
+        $response->headers->setCookie(cookie: Cookie::create(name: 'app_length', value: $length, expire: new DateTimeImmutable(datetime: '+5 years')));
 
-        $response->headers->setCookie(Cookie::create('app_uppercase_letters', $UppercaseLetters ? '1' : '0', new DateTimeImmutable('+5 years')));
+        $response->headers->setCookie(cookie: Cookie::create(name: 'app_uppercase_letters', value: $UppercaseLetters ? '1' : '0', expire: new DateTimeImmutable(datetime: '+5 years')));
 
-        $response->headers->setCookie(Cookie::create('app_digits', $digits ? '1' : '0', new DateTimeImmutable('+5 years')));
+        $response->headers->setCookie(cookie: Cookie::create(name: 'app_digits', value: $digits ? '1' : '0', expire: new DateTimeImmutable(datetime: '+5 years')));
 
-        $response->headers->setCookie(Cookie::create('app_special_characters', $SpecialCharacters ? '1' : '0', new DateTimeImmutable('+5 years')));
+        $response->headers->setCookie(cookie: Cookie::create(name: 'app_special_characters', value: $SpecialCharacters ? '1' : '0', expire: new DateTimeImmutable(datetime: '+5 years')));
 
         return $response;
     }
