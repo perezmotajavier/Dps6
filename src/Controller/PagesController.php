921f8381ea8febeeb39dc1a407a7d62a87445616
perezmotajavier <gabyperezmota@icloud.com>
2022-08-21 18:28:25 +0200

    [WIP] Project updated

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Cookie;
-
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
